72c28cfcac0a0b330957b7a02da5d3022d20569d
Juan Sáez García <contacto@juamber.com>
2021-12-26 16:13:08 +0100

    es7

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+// USAR EL PLUGIN "CODE RUNNNER" E IR EJECUTANDO TROZOS DE CÓDIGO
+
+//includes
+let numbers = [1,2,3,6,5,8];
+
+if (numbers.includes(5)){
+    console.log("Si se encuentra el valor 5");
+}else{
+    console.log("No se encuentra");
+}
+//Elevar al exponente
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
